08b771f42cbb3b7cd3235f5aab4ffe5137d0cf55
Zouth <112033816+Zouthh@users.noreply.github.com>
2023-04-16 14:24:36 -0600

    Add files via upload

---
+++ Budda.py
@@ -0,0 +1,24 @@
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+print(Fore.CYAN + "                           _")
+print("                        _ooOoo_")
+print("                       o8888888o")
+print("                       88\" . \"88")       
+print(Fore.RED + "                       (| -_- |)")    
+print(Fore.CYAN +"                       O\  =  /O")
+print(Fore.RED +"                    ____/`---'\____   --By budda")      
+print(Fore.CYAN +"                  .'  \\|     |//  `.")
+print("                 /  \\|||  :  |||//  \\")
+print("                /  _||||| -:- |||||_  \\")
+print("                |   | \\\  -  /'| |   |")
+print(Fore.RED +"                | \_|  `\`---'//  |_/ |")
+print(Fore.CYAN +"                \  .-\__ `-. -'__/-.  /")
+print("              ___`. .'  /--.--\  `. .'___")
+print("           .\"\" '<  `.___\_<|>_/___.' _> \"" + Fore.RESET)
+print(Fore.CYAN + "          | | :  `- \`. ;`. _/; .'/ /  .' ; |")  
+print("          \  \ `-.   \_\_`. _.'_/_/  -' _.' /")
+print("===========`-.`___`-.__\ \___  /__.-'_.'_.-'================")
+print("                        `=--=-'")
